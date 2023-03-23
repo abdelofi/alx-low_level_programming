675b07c0d15bb312051468b58263e305cc5bb826
abdelofi <eloufirabdelaati@gmail.com>
2023-03-23 09:15:47 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    printf("_putchar\n");
-    return 0;
+    _putchar('\n');
+    return (0);
 }
