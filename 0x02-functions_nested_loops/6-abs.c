7faa51bd0b739eeddaafea38a9124c0c4b59d0ed
abdelofi <eloufirabdelaati@gmail.com>
2023-03-23 19:03:47 -0700

    alphabet

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,19 +1,14 @@
 #include "main.h"
 #include <stdio.h>
-
 int main(void)
 {
 int r;
-
 r = _abs(-1);
 printf("%d\n", r);  // Output: 1
-
 r = _abs(0);
 printf("%d\n", r);  // Output: 0
-
 r = _abs(1);
 printf("%d\n", r);  // Output: 1
-
 r = _abs(-98);
 printf("%d\n", r);  // Output: 98
 return (0);
