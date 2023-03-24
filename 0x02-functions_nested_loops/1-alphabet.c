25ce26e158fffec6799b9cb47d0dbd94fab65628
abdelofi <eloufirabdelaati@gmail.com>
2023-03-23 18:15:36 -0700

    updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,18 @@
-#include "main.h"
+#include "main.h" // include the header file that contains the function prototype
 
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a newline
  */
 void print_alphabet(void)
 {
-  char c;
+  char c; // declare a variable to store each character of the alphabet
 
+  // iterate over each character of the alphabet from 'a' to 'z'
   for (c = 'a'; c <= 'z'; c++)
   {
-    _putchar(c);
+    _putchar(c); // print the current character to the standard output
   }
+
+  // print a newline character to the standard output
   write(1, "\n", 1);
 }
