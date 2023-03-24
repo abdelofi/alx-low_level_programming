53dad09f274e46754b7e5acefa48b05f18e65148
abdelofi <eloufirabdelaati@gmail.com>
2023-03-23 19:38:12 -0700

    updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,11 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit = n % 10;
+int last_digit = n % 10;
 
-    if (last_digit < 0)
-        last_digit = -last_digit;
+if (last_digit < 0)
+last_digit = -last_digit;
 
-    _putchar(last_digit + '0');
-
-    return (last_digit);
+_putchar(last_digit + '0');
+return (last_digit);
 }
