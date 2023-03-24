85075b5eeb61ff00bd2bd284a636830ba2546f3e
abdelofi <eloufirabdelaati@gmail.com>
2023-03-23 18:32:58 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,12 @@
 #include "main.h"
+/**
+ * print_alphabet - prints the lowercase alphabet from a to z
+ *
+ * Return: void
+ */
 void print_alphabet(void)
 {
+	/* implementation code */
 char letter = 'a';
 while (letter <= 'z')
 {
