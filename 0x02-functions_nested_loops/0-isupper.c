942cb392e10ab5750f2472f6c6301e2c408b3d8b
abdelofi <eloufirabdelaati@gmail.com>
2023-03-24 11:48:27 -0700

    updated

--- 0x02-functions_nested_loops/0-isupper.c
+++ 0x02-functions_nested_loops/0-isupper.c
@@ -1,6 +1,8 @@
+#include "main.h"
+
 /**
- * _isupper - checks for uppercase character
- * @c: character to check
+ * _isupper - checks if a character is uppercase
+ * @c: the character to be checked
  *
  * Return: 1 if c is uppercase, 0 otherwise
  */
