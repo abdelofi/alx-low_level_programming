3e369ab301c1cf12e7687358afec5c7d7bd4e4a9
abdelofi <eloufirabdelaati@gmail.com>
2023-03-23 20:11:36 -0700

    updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,17 +9,16 @@
  */
 void print_to_98(int n)
 {
-  int i;
-
-  if (n <= 98)
-  {
-    for (i = n; i <= 97; i++)
-      printf("%d, ", i);
-  }
-  else
-  {
-    for (i = n; i >= 99; i--)
-      printf("%d, ", i);
-  }
-  printf("98\n");
+int i;
+if (n <= 98)
+{
+for (i = n; i <= 97; i++)
+printf("%d, ", i);
+}
+else
+{
+for (i = n; i >= 99; i--)
+printf("%d, ", i);
+}
+printf("98\n");
 }
