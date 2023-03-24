6c82785dd9b5e9be927d86f641620aeabf96f1a4
abdelofi <eloufirabdelaati@gmail.com>
2023-03-23 19:19:33 -0700

    alphabet

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,21 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @n: the number to extract the last digit from
+ * @n: the number to get the last digit from
  *
  * Return: the value of the last digit
  */
 int print_last_digit(int n)
 {
-    int last_digit = n % 10;
+	int last_digit;
 
-    if (last_digit < 0)
-        last_digit = -last_digit;
+	if (n < 0)
+	{
+		n *= -1;
+	}
 
-    _putchar(last_digit + '0');
+	last_digit = n % 10;
+	_putchar(last_digit + '0');
 
-    return last_digit;
+	return (last_digit);
 }
