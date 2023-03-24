a1a13a03c3b682bd3371cc58b98c35a130aea51f
abdelofi <eloufirabdelaati@gmail.com>
2023-03-24 12:25:23 -0700

    updated

--- 0x02-functions_nested_loops/0-isupper.c
+++ 0x02-functions_nested_loops/0-isupper.c
@@ -5,11 +5,11 @@
  * @c: the character to be checked
  *
  * Return: 1 if c is uppercase, 0 otherwise
- */
+ * */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return (1);
-    else
-        return (0);
+ if (c >= 'A' && c <= 'Z')
+ return (1);
+ else
+ return (0);
 }
