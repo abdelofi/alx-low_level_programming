03efa86ed86f01d6300f27cc90ecc62596b3028e
abdelofi <eloufirabdelaati@gmail.com>
2023-03-25 00:46:51 -0700

    fibonacci updated

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,22 +5,20 @@
  *
  * Return: Always 0
  */
-    int main(void)
-    {
-    int i;
-    long int a = 1, b = 2, c;
-
-    _putchar('1');
-    for (i = 2; i <= 98; i++)
-    {
-    _putchar(',');
-    _putchar(' ');
-    _putchar(a + '0');
-    c = a + b;
-    a = b;
-    b = c;
-    }
-    _putchar('\n');
-
-    return (0);
+int main(void)
+{
+int i;
+long int a = 1, b = 2, c;
+_putchar('1');
+for (i = 2; i <= 98; i++)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(a + '0');
+c = a + b;
+a = b;
+b = c;
+}
+_putchar('\n');
+return (0);
 }
