8dee4a84a8d19104b2c333f4b9319e518acd28a8
abdelofi <eloufirabdelaati@gmail.com>
2023-03-25 00:08:51 -0700

    updated

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,22 +7,19 @@
  */
 int main(void)
 {
-    unsigned long int fib1 = 1, fib2 = 2, fib_sum = 0, even_sum = 0;
+    long int fib1 = 1, fib2 = 2, fib_sum = 0, even_sum = 0;
 
-    while (fib_sum < 4000000)
+    while (fib2 <= 4000000)
     {
+        if (fib2 % 2 == 0)
+            even_sum += fib2;
+
         fib_sum = fib1 + fib2;
-
-        if (fib_sum % 2 == 0)
-        {
-            even_sum += fib_sum;
-        }
-
         fib1 = fib2;
         fib2 = fib_sum;
     }
 
-    printf("%lu\n", even_sum);
+    printf("%ld\n", even_sum);
 
-    return (0);
+    return 0;
 }
