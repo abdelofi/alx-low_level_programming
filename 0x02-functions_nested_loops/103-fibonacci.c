56d681b2b6410ad52a699f02607abdce062cae2e
abdelofi <eloufirabdelaati@gmail.com>
2023-03-25 00:14:40 -0700

    fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    int term1 = 1, term2 = 2, next_term, sum = 2;
-
-    while (term2 < 4000000)
-    {
-        next_term = term1 + term2;
-        term1 = term2;
-        term2 = next_term;
-
-        if (term2 % 2 == 0)
-        {
-            sum += term2;
-        }
-    }
-
-    printf("%d\n", sum);
-
-    return (0);
+int term1 = 1, term2 = 2, next_term, sum = 2;
+while (term2 < 4000000)
+{
+next_term = term1 + term2;
+term1 = term2;
+term2 = next_term;
+if (term2 % 2 == 0)
+{
+sum += term2;
+}
+}
+printf("%d\n", sum);
+return (0);
 }
