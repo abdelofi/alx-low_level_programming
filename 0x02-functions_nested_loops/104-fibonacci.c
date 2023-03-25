eab01a4063559a7a292a0dbea647563edec7783e
abdelofi <eloufirabdelaati@gmail.com>
2023-03-25 00:40:43 -0700

    fibonacci updated

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,60 +1,26 @@
 #include "main.h"
 
 /**
- * print_fibonacci - prints the first 98 Fibonacci numbers
+ * main - Entry point
  *
- * Return: void
+ * Return: Always 0
  */
-void print_fibonacci(void)
+int main(void)
 {
     int i;
+    long int a = 1, b = 2, c;
 
-    for (i = 1; i <= 98; i++)
+    _putchar('1');
+    for (i = 2; i <= 98; i++)
     {
-        if (i == 1)
-            _putchar('1');
-        else if (i == 2)
-            _putchar('2');
-        else
-        {
-            int first = 1, second = 2, next = first + second, j;
-
-            for (j = 3; j <= i; j++)
-            {
-                first = second;
-                second = next;
-                next = first + second;
-            }
-
-            print_number(next);
-        }
-
-        if (i != 98)
-        {
-            _putchar(',');
-            _putchar(' ');
-        }
+        _putchar(',');
+        _putchar(' ');
+        _putchar(a + '0');
+        c = a + b;
+        a = b;
+        b = c;
     }
-
     _putchar('\n');
-}
 
-/**
- * print_number - prints an integer
- * @n: the integer to print
- *
- * Return: void
- */
-void print_number(int n)
-{
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
-
-    if (n / 10)
-        print_number(n / 10);
-
-    _putchar(n % 10 + '0');
+    return (0);
 }
