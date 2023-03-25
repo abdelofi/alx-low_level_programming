eeaa5f2aac98e3d88850c119e3a2b1f503a87450
abdelofi <eloufirabdelaati@gmail.com>
2023-03-25 00:29:57 -0700

    fibonacci updated

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,34 +1,16 @@
-#include "main.h"
-
-/**
- * print_fibonacci - prints the first 98 Fibonacci numbers
- *
- * Return: void
- */
-void print_fibonacci(void)
-{
-    int i;
-    unsigned long int num1 = 1, num2 = 2, sum;
-
-    _putchar('1');
-    for (i = 2; i <= 98; i++)
-    {
-        _putchar(',');
-        _putchar(' ');
-        _putchar(num2 / 1000000000 + '0');
-        _putchar((num2 / 100000000) % 10 + '0');
-        _putchar((num2 / 10000000) % 10 + '0');
-        _putchar((num2 / 1000000) % 10 + '0');
-        _putchar((num2 / 100000) % 10 + '0');
-        _putchar((num2 / 10000) % 10 + '0');
-        _putchar((num2 / 1000) % 10 + '0');
-        _putchar((num2 / 100) % 10 + '0');
-        _putchar((num2 / 10) % 10 + '0');
-        _putchar(num2 % 10 + '0');
-        sum = num1 + num2;
-        num1 = num2;
-        num2 = sum;
-    }
-    _putchar('\n');
+#include <stdio.h>
+int main(void) {
+int a = 1, b = 2, c, i;
+printf("%d, %d, ", a, b);
+for (i = 3; i <= 98; i++) {
+c = a + b;
+printf("%d", c);
+if (i < 98) {
+printf(", ");
 }
+a = b;
+b = c;
+}
+printf("\n");
+return 0;
 }
