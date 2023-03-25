07a25850260f4ed542b67cb678ce2f923ea9f49b
abdelofi <eloufirabdelaati@gmail.com>
2023-03-25 00:26:54 -0700

    fibonacc updated

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,43 +1,34 @@
 #include "main.h"
 
 /**
- * fibonacci - Prints the first 98 Fibonacci numbers.
+ * print_fibonacci - prints the first 98 Fibonacci numbers
+ *
+ * Return: void
  */
-void fibonacci(void)
+void print_fibonacci(void)
 {
     int i;
-    unsigned long num1 = 1, num2 = 2, fib;
+    unsigned long int num1 = 1, num2 = 2, sum;
 
     _putchar('1');
-    _putchar(',');
-    _putchar(' ');
-    _putchar('2');
-    _putchar(',');
-    _putchar(' ');
-
-    for (i = 0; i < 96; i++)
+    for (i = 2; i <= 98; i++)
     {
-        fib = num1 + num2;
-        _putchar(fib / 1000000000 + '0');
-        _putchar(fib / 100000000 % 10 + '0');
-        _putchar(fib / 10000000 % 10 + '0');
-        _putchar(fib / 1000000 % 10 + '0');
-        _putchar(fib / 100000 % 10 + '0');
-        _putchar(fib / 10000 % 10 + '0');
-        _putchar(fib / 1000 % 10 + '0');
-        _putchar(fib / 100 % 10 + '0');
-        _putchar(fib / 10 % 10 + '0');
-        _putchar(fib % 10 + '0');
-
-        if (i != 95)
-        {
-            _putchar(',');
-            _putchar(' ');
-        }
-
+        _putchar(',');
+        _putchar(' ');
+        _putchar(num2 / 1000000000 + '0');
+        _putchar((num2 / 100000000) % 10 + '0');
+        _putchar((num2 / 10000000) % 10 + '0');
+        _putchar((num2 / 1000000) % 10 + '0');
+        _putchar((num2 / 100000) % 10 + '0');
+        _putchar((num2 / 10000) % 10 + '0');
+        _putchar((num2 / 1000) % 10 + '0');
+        _putchar((num2 / 100) % 10 + '0');
+        _putchar((num2 / 10) % 10 + '0');
+        _putchar(num2 % 10 + '0');
+        sum = num1 + num2;
         num1 = num2;
-        num2 = fib;
+        num2 = sum;
     }
-
     _putchar('\n');
+}
 }
