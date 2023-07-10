8833b02bee249c8e636165776e7d087d82d5267c
abdelofi <eloufirabdelaati@gmail.com>
2023-07-10 15:59:14 -0700

    updated

--- 0-read_textfile.c/0-read_textfile.c
+++ 0-read_textfile.c/0-read_textfile.c
@@ -5,47 +5,45 @@
 
 /**
  * read_textfile - Read text file and print to STDOUT.
- * @filename: Name of the text file being read.
+ * @filename: Text file to be read.
  * @letters: Number of letters to be read.
- * Return: Number of bytes read and printed, or 0 on failure or if filename is NULL.
+ *
+ * Return: Number of bytes read and printed.
+ *         0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t w, t;
-	char *buf;
+    char *buf;
+    ssize_t fd;
+    ssize_t w;
+    ssize_t t;
 
-	if (filename == NULL)
-		return (0);
+    if (filename == NULL)
+        return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+    buf = malloc(sizeof(char) * letters);
+    if (buf == NULL)
+    {
+        close(fd);
+        return (0);
+    }
 
-	t = read(fd, buf, letters);
-	if (t == -1)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
+    t = read(fd, buf, letters);
+    if (t == -1)
+    {
+        free(buf);
+        close(fd);
+        return (0);
+    }
 
-	w = write(STDOUT_FILENO, buf, t);
-	if (w == -1 || w != t)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
+    w = write(STDOUT_FILENO, buf, t);
 
-	free(buf);
-	close(fd);
-	return (w);
+    free(buf);
+    close(fd);
+
+    return (w);
 }
